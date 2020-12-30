29e412383a3a58527a701d08fbb38db6a7ae7904
Satoru SATOH <satoru.satoh@gmail.com>
2020-12-30 10:24:41 +0900

    refactor: simplify the test case

--- tests/test_check_file_has_some_contents.py
+++ tests/test_check_file_has_some_contents.py
@@ -1,18 +1,13 @@
 # Copyright (C) 2020 Red Hat, Inc.
 # SPDX-License-Identifier: MIT
 #
-# pylint: disable=missing-function-docstring,too-few-public-methods
 """Tests for the hook.
 """
 from tests import common as C
 
 
-class Base(C.BaseTestCase):
-    """Base class."""
-    filename = C.basename(__file__)
-
-
-class TestCase(Base, C.RunHookTestCase):
+class TestCase(C.RunHookTestCase):
     """Hook test cases."""
+    filename = C.basename(__file__)
 
 # vim:sw=4:ts=4:et:
